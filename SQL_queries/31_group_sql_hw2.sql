dd3cc7ea2b9914053af29f7586fec279f6142522
Ruslan Gadaborshev <qa.gadaborshev@gmail.com>
2023-01-22 16:45:36 +0300

    Finisheded second homework in my SQL-learning way

--- SQL_queries/31_group_sql_hw2.sql
+++ SQL_queries/31_group_sql_hw2.sql
@@ -136,175 +136,177 @@ values
   (2500);
 
 
-
-
-
-
-
-
-Таблица employee_salary
-
-Создать таблицу employee_salary
+/*Создать таблицу employee_salary
 - id. Serial  primary key,
 - employee_id. Int, not null, unique
 - salary_id. Int, not null
 Наполнить таблицу employee_salary 40 строками:
-- в 10 строк из 40 вставить несуществующие employee_id
+- в 10 строк из 40 вставить несуществующие employee_id*/
 
-id
-employee_id
-salary_id
-1
-3
-7
-2
-1
-4
-3
-5
-9
-4
-40
-13
-5
-23
-4
-6
-11
-2
-7
-52
-10
-8
-15
-13
-9
-26
-4
-10
-16
-1
-11
-33
-7
-...
-...
-...
+create table employee_salary(
+  id serial primary key, employee_id int not null unique, 
+  salary_id int not null
+);
+select 
+  * 
+from 
+  employee_salary;
+
+insert into employee_salary(employee_id, salary_id) 
+values 
+  (3, 7), 
+  (1, 4), 
+  (5, 9), 
+  (40, 13), 
+  (23, 4), 
+  (11, 2), 
+  (52, 11), 
+  (15, 13), 
+  (26, 4), 
+  (16, 1), 
+  (33, 7), 
+  (69, 2), 
+  (70, 1), 
+  (68, 2), 
+  (66, 3), 
+  (64, 4), 
+  (62, 5), 
+  (60, 6), 
+  (58, 7), 
+  (56, 8), 
+  (54, 9), 
+  (51, 10), 
+  (50, 11), 
+  (48, 12), 
+  (46, 13), 
+  (44, 14), 
+  (42, 15), 
+  (41, 16), 
+  (38, 14), 
+  (36, 12), 
+  (71, 1), 
+  (73, 2), 
+  (75, 3), 
+  (77, 4), 
+  (79, 5), 
+  (72, 6), 
+  (74, 10), 
+  (76, 11), 
+  (78, 12), 
+  (80, 13);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-Таблица roles
-
-Создать таблицу roles
+/*Создать таблицу roles
 - id. Serial  primary key,
 - role_name. int, not null, unique
 Поменять тип столба role_name с int на varchar(30)
-Наполнить таблицу roles 20 строками:
-
-id
-role_name
-1
-Junior Python developer
-2
-Middle Python developer
-3
-Senior Python developer
-4
-Junior Java developer
-5
-Middle Java developer
-6
-Senior Java developer
-7
-Junior JavaScript developer
-8
-Middle JavaScript developer
-9
-Senior JavaScript developer
-10
-Junior Manual QA engineer
-11
-Middle Manual QA engineer
-12
-Senior Manual QA engineer
-13
-Project Manager
-14
-Designer
-15
-HR
-16
-CEO
-17
-Sales manager
-18
-Junior Automation QA engineer
-19
-Middle Automation QA engineer
-20
-Senior Automation QA engineer
+Наполнить таблицу roles 20 строками:*/
 
 
 
-Таблица roles_employee
+ create table roles(
+  id serial primary key, role_name int not null unique
+);
+ALTER TABLE 
+  roles ALTER COLUMN role_name VARCHAR(30); -- отработало с ошибкой, пошел пути дропа 
+drop 
+  table roles;
+create table roles(
+  id serial primary key, 
+  role_name VARCHAR(30) not null unique
+);
+select 
+  * 
+from 
+  roles;
+insert into roles(role_name) 
+values 
+  ('Junior Python developer'), 
+  ('Middle Python developer'), 
+  ('Senior Python developer'), 
+  ('Junior Java developer'), 
+  ('Middle Java developer'), 
+  ('Senior Java developer'), 
+  ('Junior JavaScript developer'), 
+  ('Middle JavaScript developer'), 
+  ('Senior JavaScript developer'), 
+  ('Junior Manual QA engineer'), 
+  ('Middle Manual QA engineer'), 
+  ('Senior Manual QA engineer'), 
+  ('Project Manager'), 
+  ('Designer'), 
+  ('HR'), 
+  ('CEO'), 
+  ('Sales manager'), 
+  (
+    'Junior Automation QA engineer'
+  ), 
+  (
+    'Middle Automation QA engineer'
+  ), 
+  (
+    'Senior Automation QA engineer'
+  );
 
-Создать таблицу roles_employee
+
+/* Создать таблицу roles_employee
 - id. Serial  primary key,
 - employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
 - role_id. Int, not null (внешний ключ для таблицы roles, поле id)
-Наполнить таблицу roles_employee 40 строками:
+Наполнить таблицу roles_employee 40 строками:*/
 
-id
-employee_id
-role_id
-1
-7
-2
-2
-20
-4
-3
-3
-9
-4
-5
-13
-5
-23
-4
-6
-11
-2
-7
-10
-9
-8
-22
-13
-9
-21
-3
-10
-34
-4
-11
-6
-7
-...
-...
-...
+ create table roles_employee(
+  id serial primary key, employee_id int not null unique, 
+  role_id int not null
+);
+select 
+  * 
+from 
+  roles_employee;
+insert into roles_employee (employee_id, role_id) 
+values 
+  (7, 2), 
+  (20, 4), 
+  (3, 9), 
+  (5, 13), 
+  (23, 4), 
+  (11, 2), 
+  (10, 9), 
+  (22, 13), 
+  (21, 3), 
+  (34, 4), 
+  (6, 7), 
+  (40, 1), 
+  (41, 2), 
+  (42, 3), 
+  (43, 4), 
+  (44, 5), 
+  (45, 6), 
+  (46, 7), 
+  (47, 8), 
+  (48, 9), 
+  (49, 10), 
+  (50, 11), 
+  (51, 12), 
+  (52, 13), 
+  (53, 14), 
+  (54, 15), 
+  (55, 16), 
+  (57, 1), 
+  (58, 2), 
+  (59, 3), 
+  (60, 4), 
+  (61, 5), 
+  (62, 6), 
+  (63, 7), 
+  (64, 8), 
+  (65, 9), 
+  (66, 10), 
+  (67, 11), 
+  (68, 12), 
+  (69, 13);
+
 
 
 
